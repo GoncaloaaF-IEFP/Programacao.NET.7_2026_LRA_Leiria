56c04783fd5e58e42dcc5088fba2aa77a78f1d0e
GoncaloaaF <goncaloaaf@gmail.com>
2026-05-06 12:18:31 +0100

    base aula 1

---
+++ UC0809/01_demo1/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+
+    int idade = 10;
+    float salario = 12.231f;
+    double valor = 12.231;
+    // char gen = 'M';
+
+    long num  = 123;
+
+    // float vs double
+
+    // int ->  -2,147,483,648               e 2,147,483,647
+    // long -> -9,223,372,036,854,775,808	e 9,223,372,036,854,775,807
+
+
+    int ano = 2026;
+    printf("Ola Mundo, %d\n", ano);
+    printf("nova\tlinha\n");
+
+
+    return 0;
+}
