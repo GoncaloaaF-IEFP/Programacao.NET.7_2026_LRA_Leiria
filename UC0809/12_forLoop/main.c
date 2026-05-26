05abfc79e98127476a37bbbb42921a2dc871586c
GoncaloaaF <goncaloaaf@gmail.com>
2026-05-26 14:37:33 +0100

    intro for loop

---
+++ UC0809/12_forLoop/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello, World!\n");
+
+    int i = 3;
+
+    for (int i = 1; i <= 1024; i*=2 ) {
+        printf("%d\n", i);
+    }
+
+    printf("-->%d<--\n", i);
+
+    /*
+     * Crie uma função que receba um valor e mostra a tabuada desse valor
+     * Garanta que o número é maior que 0
+     *
+     *
+     */
+
+
+    return 0;
+}
